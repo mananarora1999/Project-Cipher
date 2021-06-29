972bc656926e0ba30c52609abd5de4eed1c2fe61
mananarora1999 <86541334+mananarora1999@users.noreply.github.com>
2021-06-29 09:01:17 +0530

    Add files via upload

---
+++ CIPHER.java
@@ -0,0 +1,16 @@
+import PROJECT.PROJECTCIPHER;
+
+public class CIPHER 
+{
+    private CIPHER()
+    {
+        // CONSTRUCTOR
+    }
+    
+    public static void main (String []args)
+    {
+      PROJECTCIPHER project = new PROJECTCIPHER();
+      project.main(args);
+    }
+}
+
